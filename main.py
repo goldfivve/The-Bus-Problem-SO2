e60662682a3d1fcf2c44e3dfd7353c6f565d141f
goldfivve <dominika.kmiecik6@gmail.com>
2022-04-03 15:27:23 +0200

    add passenger and bus threads

--- main.py
+++ main.py
@@ -2,50 +2,45 @@ import logging
 from threading import Thread
 import time
 from time import sleep
-
 from duplicity.asyncscheduler import threading
 
 
 class BusStop:
     indexes = [1, 2, 3]
-    riders_number: int
+    passengers_number: int
     index: int
 
     def __init__(self):
-        self.riders_number = 0
+        self.passengers_number = 0
         self.index = 1
-
-    def update(self):
-        self.riders_number += 1
 
 
 class Bus:
     positions = ["arriving", "stop", "departed"]
     position: str
     full: bool
-    bus_stop: BusStop
-    riders: int
+    passengers: int
 
-    def __init__(self, position, full, bus_stop: BusStop):
-        self.position = position
-        self.full = full
-        self.bus_stop = bus_stop
+    def __init__(self):
+        self.position = self.positions[0]
+        self.full = False
+        self.passengers = 0
         self._lock = threading.Lock()
 
-    def drive(self, bus_stop: BusStop):
+    def drive(self, active_bus_stop: BusStop):
         print("Bus arriving")
         sleep(5)
-        if bus_stop.riders_number == 0:
-            self.position = self.positions[2]
-        elif bus_stop.riders_number is not 0:
-            self.position = self.position[1]
+        if active_bus_stop.passengers_number != 0 and not self.full:
+            self.position = self.positions[1]
+            while self.passengers != active_bus_stop.passengers_number:
+                print("Waiting for all passengers boarded")
+                sleep(5)
 
-        with self._lock:
-            if self.riders == BusStop.riders_number:
-                self.position = self.positions[2]
+        print("Bus departing...")
+        self.position = self.positions[2]
 
 
-class Rider:
+class Passenger:
     on_bus_stop: bool
     bus_ticket: bool
     inside: bool
@@ -57,32 +52,40 @@ class Rider:
         self.inside = False
         self._lock = threading.Lock()
 
-    def arrive_bus_stop(self, bus: Bus):
+    def arrive_bus_stop(self, active_bus: Bus, active_bus_stop: BusStop):
         if not self.on_bus_stop:
-            logging.info("Thread %d: arriving bus stop", self.name)
-            self._lock.acquire()
+            logging.info("Passenger arriving bus stop")
             time.sleep(2)
             self.on_bus_stop = True
             if self.bus_ticket is True:
-                BusStop.riders_number += 1
-            self._lock.release()
-            logging.info("Thread %d: arrived bus stop", self.name)
+                active_bus_stop.passengers_number += 1
+            logging.info("Passenger arrived bus stop")
 
-        if bus.position is "arriving" and self.on_bus_stop is True:
-            print("Rider can board the bus")
-            if bus.position is "stop":
-                with self._lock:
-                    if self.bus_ticket and bus.riders < 50:
-                        bus.riders += 1
+        while not self.inside:
+            if active_bus.position == "arriving" and self.on_bus_stop:
+                print("Passenger can board the bus")
+                while not active_bus.positions[1]:
+                    print("Waiting for the bus to stop")
+                if active_bus.position == "stop":
+                    print("Passenger boarding bus")
+                    if self.bus_ticket and active_bus.passengers < 50:
+                        active_bus.passengers += 1
                         self.inside = True
-                    # else:
-                    # zakoncz watek
+                        print("Passenger boarded")
+                    else:
+                        print("Keep waiting")
 
 
 if __name__ == '__main__':
     print('PyCharm')
-    rider_thread = Thread(target=Rider.arrive_bus_stop, args=(False, True, False))
-    bus_thread = Thread(target=Bus.drive)
+    bus_stop = BusStop()
+    passenger = Passenger()
+    bus = Bus()
+    passenger_thread = Thread(target=passenger.arrive_bus_stop, args=(bus, bus_stop,))
+    bus_thread = Thread(target=bus.drive, args=(bus_stop,))
 
-    rider_thread.join()
+    passenger_thread.start()
+    bus_thread.start()
+
+    passenger_thread.join()
     bus_thread.join()
