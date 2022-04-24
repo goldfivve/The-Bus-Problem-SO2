9fb9090a357285da6594dc547d205f9d556a574b
goldfivve <dominika.kmiecik6@gmail.com>
2022-04-25 00:37:28 +0200

    add bus lines and fix ticket inspection

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 from threading import Thread
 import time
 from time import sleep
@@ -7,7 +6,6 @@ import random
 
 
 class BusStop:
-    indexes = [1, 2, 3]
     passengers_number: int
     index: int
 
@@ -17,11 +15,12 @@ class BusStop:
 
 
 class Bus:
-    positions = ["arriving", "stop", "departed", "based"]
+    positions = ("arriving", "stop", "departed", "based")
     position: str
-    full: bool
+    line: int
     passengers: int
     ticket_inspector: bool
+    check: float
 
     def __init__(self):
         self.position = self.positions[3]
@@ -29,21 +28,26 @@ class Bus:
         self.passengers = 0
         self._lock = threading.Lock()
         self.ticket_inspector = False
+        self.line = random.randint(1, 3)
+        self.check = random.random()
 
     def drive(self, active_bus_stop: BusStop):
-
-        print("Bus arriving")
+        print(self.check)
+        print(f"Bus line {self.line} arriving")
         self.position = self.positions[0]
+        if self.check > 0.1:
+            print("Ticket inspector is coming!")
+            self.ticket_inspector = True
+
         sleep(10)
+
         if active_bus_stop.passengers_number != 0 and not self.full:
             self.position = self.positions[1]
-            if random.random() < 0.1:
-                print("Ticket inspector is coming!")
-                self.ticket_inspector = True
             while active_bus_stop.passengers_number > 0:
                 print("Waiting for all passengers boarded")
                 sleep(10)
 
+        print(f"passenger number: {self.passengers}")
         print("Bus departing...")
         self.position = self.positions[2]
 
@@ -53,41 +57,45 @@ class Passenger:
     bus_ticket: bool
     inside: bool
     name: int
-    first: int
+    first: bool
+    line: int
+    cheat: float
 
     def __init__(self):
         self.on_bus_stop = False
         self.bus_ticket = False
         self.inside = False
-        self.first = 0
+        self.first = True
         self._lock = threading.Lock()
+        self.line = random.randint(1, 3)
+        self.cheat = random.random()
 
     def arrive_bus_stop(self, active_bus: Bus, active_bus_stop: BusStop):
+        sleep(3)
         if not self.on_bus_stop:
-            logging.info("Passenger arriving bus stop")
+            print(f"Passenger arriving bus stop for line {self.line}")
             time.sleep(2)
             self.on_bus_stop = True
-            logging.info("Passenger arrived bus stop")
+            print("Passenger arrived bus stop")
 
-        if random.random() > 0.05:
+        if self.cheat < 0.05:
             with self._lock:
                 self.bus_ticket = True
 
         while active_bus.ticket_inspector and not self.bus_ticket:
-            if self.first == 0:
+            if self.first:
                 print("Oops! Ticket inspector caught passenger red-handed... 280 pln 😎😩")
-                self.first = 1
+                self.first = False
 
         while not self.inside:
-            if active_bus.position == active_bus.positions[0] and self.on_bus_stop:
+            if active_bus.position == active_bus.positions[0] and self.on_bus_stop and self.line == active_bus.line:
                 active_bus_stop.passengers_number += 1
-                if self.first == 0:
+                if not self.first:
                     print("Passenger can board the bus")
-                    self.first = 1
                 while not active_bus.position == active_bus.positions[1]:
-                    if self.first == 0:
+                    if not self.first:
                         print("Waiting for the bus to stop")
-                        self.first = 1
+                        self.first = True
                 print("Passenger boarding bus")
                 if active_bus.passengers < 50:
                     with self._lock:
@@ -107,7 +115,7 @@ if __name__ == '__main__':
     bus_thread = Thread(target=bus.drive, args=(bus_stop,))
 
     passengers = list()
-    for index in range(51):
+    for index in range(55):
         passenger = Passenger()
         passenger_thread = Thread(target=passenger.arrive_bus_stop, args=(bus, bus_stop))
         passengers.append((passenger_thread, passenger))
